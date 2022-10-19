75cc3da1c023be8a6a56fdda74768a8dfc04d920
aidenlx <31102694+aidenlx@users.noreply.github.com>
2022-10-19 13:21:46 +0800

    docs(docs-site): disable search engine index before docs completes

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
-
+  noIndex: true,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
