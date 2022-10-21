f80dbf04b89cd51c97a7e8838811dd0c5501e727
aidenlx <31102694+aidenlx@users.noreply.github.com>
2022-10-21 13:49:02 +0800

    docs(getting-started): add warning for BRAT install; auto choose available install method

--- docs/src/components/brat-check.tsx
+++ docs/src/components/brat-check.tsx
@@ -1,30 +1,21 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import BrowserOnly from "@docusaurus/BrowserOnly";
-import React, { useState, useEffect } from "react";
-import { Availablity, AvailablityTag } from "./available";
+import React from "react";
+import {
+  Availablity,
+  AvailablityTag,
+  betaManifest,
+  useManifest,
+} from "./available";
 
 export default function BRATAvailable() {
   return (
     <BrowserOnly fallback={<span>{Availablity.checking}</span>}>
       {() => {
-        const [available, setAvailable] = useState(Availablity.checking);
-        useEffect(() => {
-          fetch(
-            "https://raw.githubusercontent.com/aidenlx/obsidian-zotero/master/manifest-beta.json",
-          )
-            .then((res) => {
-              if (res.ok) {
-                setAvailable(Availablity.yes);
-              } else {
-                setAvailable(Availablity.no);
-              }
-            })
-            .catch((err) => {
-              console.error(err);
-              setAvailable(Availablity.unknown);
-            });
-        }, []);
-        return <AvailablityTag available={available} />;
+        const [available, versions] = useManifest(betaManifest);
+        return (
+          <AvailablityTag available={available} versions={versions} beta />
+        );
       }}
     </BrowserOnly>
   );
--- docs/src/components/ob-check.tsx
+++ docs/src/components/ob-check.tsx
@@ -1,37 +1,22 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import BrowserOnly from "@docusaurus/BrowserOnly";
-import React, { useState, useEffect } from "react";
-import { Availablity, AvailablityTag } from "./available";
+import React from "react";
+import {
+  Availablity,
+  AvailablityTag,
+  mainManifest,
+  useManifest,
+  usePluginList,
+} from "./available";
 
 export default function ObsidianAvailable() {
   return (
     <BrowserOnly fallback={<span>{Availablity.checking}</span>}>
       {() => {
-        const [available, setAvailable] = useState(Availablity.checking);
-        useEffect(() => {
-          fetch(
-            "https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/community-plugins.json",
-          )
-            .then((res) => (res.ok ? res.json() : null))
-            .then((json) => {
-              if (json && Array.isArray(json)) {
-                if (
-                  json.findIndex(
-                    (plugin) => plugin.id === "obsidian-zotero-plugin",
-                  ) !== -1
-                ) {
-                  setAvailable(Availablity.yes);
-                } else {
-                  setAvailable(Availablity.no);
-                }
-              } else setAvailable(Availablity.unknown);
-            })
-            .catch((err) => {
-              console.error(err);
-              setAvailable(Availablity.unknown);
-            });
-        }, []);
-        return <AvailablityTag available={available} />;
+        const available = usePluginList();
+        const [, versions] = useManifest(mainManifest);
+
+        return <AvailablityTag available={available} versions={versions} />;
       }}
     </BrowserOnly>
   );
