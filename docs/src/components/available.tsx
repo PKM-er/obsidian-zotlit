c9f7c6ba5ce14e011e827517218bc1ca28f286fd
aidenlx <31102694+aidenlx@users.noreply.github.com>
2022-10-21 13:49:02 +0800

    docs(getting-started): add warning for BRAT install; auto choose available install method

--- docs/src/components/available.tsx
+++ docs/src/components/available.tsx
@@ -6,19 +6,34 @@ export const enum Availablity {
 }
 
 import Admonition from "@theme/Admonition";
-import React from "react";
+import clsx from "clsx";
+import React, { useEffect, useState } from "react";
+import styles from "./available.module.css";
 
-export const AvailablityTag = (props: { available: Availablity }) => {
+export const AvailablityTag = (props: {
+  available: Availablity;
+  versions?: [ver: string, obVer: string] | null;
+  beta?: boolean;
+}) => {
   switch (props.available) {
     case Availablity.checking:
-    case Availablity.yes:
       return null;
-    case Availablity.unknown:
+    case Availablity.yes:
+    case Availablity.unknown: {
+      const [version, obVersion] = props.versions ?? ["❓", "❓"];
       return (
         <Admonition type="info">
-          Cannot check if this method is available
+          <div>
+            Latest {props.beta && "Beta "}Version:
+            <code className={clsx(styles.version)}>{version}</code>
+          </div>
+          <div>
+            Required Obsidian Version:
+            <code className={clsx(styles.version)}>{obVersion}</code>
+          </div>
         </Admonition>
       );
+    }
     case Availablity.no:
       return (
         <Admonition type="caution">This method is not yet available</Admonition>
@@ -26,4 +41,96 @@ export const AvailablityTag = (props: { available: Availablity }) => {
     default:
       break;
   }
+};
+
+export const useManifest = (url: string) => {
+  const [versions, setVersions] = useState<[ver: string, obVer: string] | null>(
+    null,
+  );
+  const [available, setAvailable] = useState(Availablity.checking);
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    fetch(url, { signal })
+      .then((res) => (res.ok ? res.json() : null))
+      .then((json) => {
+        if (!json) {
+          setAvailable(Availablity.no);
+          setVersions(null);
+        } else {
+          setAvailable(Availablity.yes);
+          const { version, minAppVersion } = json;
+          setVersions([version, minAppVersion]);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        setAvailable(Availablity.unknown);
+        setVersions(null);
+      });
+    return () => {
+      controller.abort();
+      setAvailable(Availablity.unknown);
+      setVersions(null);
+    };
+  }, [url]);
+  return [available, versions] as const;
+};
+
+export const mainManifest =
+    "https://raw.githubusercontent.com/aidenlx/obsidian-zotero/master/manifest.json",
+  betaManifest = `https://raw.githubusercontent.com/aidenlx/obsidian-zotero/master/manifest-beta.json`,
+  pluginList =
+    "https://raw.githubusercontent.com/obsidianmd/obsidian-releases/master/community-plugins.json";
+
+export const usePluginList = () => {
+  const [available, setAvailable] = useState(Availablity.checking);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    fetch(pluginList, { signal })
+      .then((res) => (res.ok ? res.json() : null))
+      .then((json) => {
+        if (json && Array.isArray(json)) {
+          if (
+            json.findIndex(
+              (plugin) => plugin.id === "obsidian-zotero-plugin",
+            ) !== -1
+          ) {
+            setAvailable(Availablity.yes);
+          } else {
+            setAvailable(Availablity.no);
+          }
+        } else setAvailable(Availablity.unknown);
+      })
+      .catch((err) => {
+        console.error(err);
+        setAvailable(Availablity.unknown);
+      });
+    return () => {
+      controller.abort();
+      setAvailable(Availablity.unknown);
+    };
+  }, []);
+  return available;
+};
+
+export const useDefaultMethod = () => {
+  const obAvailable = usePluginList();
+  const [bratAvailable] = useManifest(betaManifest);
+  let val: "brat" | "obsidian" | "manual" = "manual";
+  if (
+    bratAvailable === Availablity.checking ||
+    obAvailable === Availablity.checking
+  ) {
+    val = "obsidian";
+  }
+  if (obAvailable === Availablity.yes) {
+    val = "obsidian";
+  } else if (bratAvailable === Availablity.yes) {
+    val = "brat";
+  }
+  console.log(val);
+  return val;
 };
