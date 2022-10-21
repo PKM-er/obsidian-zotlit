51f42a738b1bddfeabcc6211d44d37337dd78c4c
aidenlx <31102694+aidenlx@users.noreply.github.com>
2022-10-21 10:29:32 +0800

    build(obsidian): update build and release script
    add `.zip` build for manual install; copy manifest to root for obsidian plugin release

--- app/obsidian/scripts/ob-bumper.mjs
+++ app/obsidian/scripts/ob-bumper.mjs
@@ -1,4 +1,6 @@
 import { promises } from "fs";
+import { copyFile } from "fs/promises";
+import { join, resolve } from "path";
 const { readFile, writeFile, stat } = promises;
 import { Plugin } from "release-it";
 
@@ -6,34 +8,76 @@ const noop = Promise.resolve();
 
 class ObsidianVersionBump extends Plugin {
   async bump(targetVersion) {
-    let { indent = 4, beta = false } = this.getContext();
+    let { indent = 4, beta = false, copyTo } = this.getContext();
     const { isDryRun } = this.config;
 
-    const targetManifest = beta ? "manifest-beta.json" : "manifest.json";
+    const mainManifest = "manifest.json",
+      betaManifest = "manifest-beta.json",
+      targetManifest = beta ? betaManifest : mainManifest;
 
-    // read minAppVersion from manifest.json and bump version to target version
-    let manifest = JSON.parse(await readFile(targetManifest, "utf8"));
+    const readJson = async (path) => {
+      try {
+        const result = JSON.parse(await readFile(path, "utf8"));
+        this.log.exec(`Reading manifest from ${path}`, isDryRun);
+        return result;
+      } catch (error) {
+        if (error.code === "ENOENT") {
+          return null;
+        }
+        throw error;
+      }
+    };
+
+    let manifest = await readJson(targetManifest);
+    if (!manifest && targetManifest === betaManifest) {
+      this.log.exec(
+        `failed to find ${betaManifest}, read from ${mainManifest} instead`,
+        isDryRun,
+      );
+      manifest = await readJson(mainManifest);
+    }
+    if (!manifest) {
+      throw new Error("manifest.json not found");
+    }
+
+    // read minAppVersion from manifest and bump version to target version
     const { minAppVersion } = manifest;
     this.log.info(`min obsidian app version: ${minAppVersion}`);
 
+    const write = async (file, content) => {
+      const tasks = [];
+      !isDryRun && tasks.push(writeFile(file, content));
+      this.log.exec(`Write to ${resolve(file)}`, isDryRun);
+      if (copyTo) {
+        const target = join(copyTo, file);
+        this.log.exec(`Copied to ${resolve(target)}`, isDryRun);
+        !isDryRun && tasks.push(writeFile(target, content));
+      }
+      await Promise.all(tasks);
+    };
+
     const writeManifest = async () => {
+      // update version
       manifest.version = targetVersion;
       const manifestContent = JSON.stringify(manifest, null, indent);
-      this.log.exec(`Writing version to manifest.json`, isDryRun);
-      !isDryRun && (await writeFile(targetManifest, manifestContent));
+      this.log.exec(`Writing version to ${targetManifest}`, isDryRun);
+      await write(targetManifest, manifestContent);
 
-      if (targetManifest === "manifest-beta.json") return noop;
-      try {
-        await stat("manifest-beta.json");
-      } catch (error) {
-        if (error.code !== "ENOENT") throw error;
-        return noop;
+      if (beta) {
+        // copy manifest.json to root
+        const target = join(copyTo, mainManifest);
+        this.log.exec(`Copied ${mainManifest} to ${resolve(target)}`, isDryRun);
+        !isDryRun && (await copyFile(mainManifest, target));
+      } else {
+        try {
+          await stat(betaManifest);
+        } catch (error) {
+          if (error.code !== "ENOENT") throw error;
+          return noop;
+        }
+        this.log.exec(`Sync ${betaManifest} with ${mainManifest}`, isDryRun);
+        await write(betaManifest, manifestContent);
       }
-      this.log.exec(
-        `Update manifest-beta.json to sync with manifest.json`,
-        isDryRun,
-      );
-      !isDryRun && (await writeFile("manifest-beta.json", manifestContent));
     };
 
     /**
@@ -43,25 +87,10 @@ class ObsidianVersionBump extends Plugin {
       let versions = JSON.parse(await readFile("versions.json", "utf-8"));
       versions[targetVersion] = minAppVersion;
       this.log.exec(`Writing version to versions.json`, isDryRun);
-      if (isDryRun) return noop;
-      return writeFile("versions.json", JSON.stringify(versions, null, indent));
+      await write("versions.json", JSON.stringify(versions, null, indent));
     };
 
-    /**
-     * update README.md with target version
-     */
-    const writeReadme = async () => {
-      let readme = await readFile("README.md", "utf-8");
-      readme = readme.replace(
-        /(?<=available for Obsidian v).*\+\.$/gm,
-        `${minAppVersion}+.`,
-      );
-      this.log.exec(`Writing version to README.md`, isDryRun);
-      if (isDryRun) return noop;
-      return writeFile("README.md", readme);
-    };
-
-    return Promise.all([writeManifest(), writeVersion(), writeReadme()]);
+    return Promise.all([writeManifest(), writeVersion()]);
   }
 }
 export default ObsidianVersionBump;
