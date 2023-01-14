5c159c563c67348b6a6b67f5497961c7cd280487
aidenlx <31102694+aidenlx@users.noreply.github.com>
2023-01-14 22:29:05 +0800

    fix(lib/database): fix query citekey fails on main lib (libId = 1)

--- lib/database/src/sql/better-bibtex.ts
+++ lib/database/src/sql/better-bibtex.ts
@@ -1,7 +1,17 @@
-import type { Transaction } from "@aidenlx/better-sqlite3";
+import type { Database, Statement, Transaction } from "@aidenlx/better-sqlite3";
 import { PreparedBase } from "../utils/index.js";
 
-const query = `--sql
+const queryMainLib = `--sql
+SELECT
+  citekey
+FROM
+  citekeys
+WHERE
+  itemID = $itemID
+  AND libraryID IS NULL
+`;
+
+const queryAltLib = `--sql
 SELECT
   citekey
 FROM
@@ -28,20 +38,30 @@ type Output = Record<number, string>;
 
 export class BetterBibtex extends PreparedBase<InputSql, OutputSql, Output> {
   trxCache: Record<number, Transaction> = {};
+
+  statementAltLib: Statement;
+  constructor(database: Database) {
+    super(database);
+    this.statementAltLib = this.database.prepare(queryAltLib);
+  }
   sql(): string {
-    return query;
+    return queryMainLib;
   }
 
-  get(input: InputSql): OutputSql | undefined {
+  getMainLib(input: InputSql): OutputSql | undefined {
     return this.statement.get(input);
+  }
+  getAltLib(input: InputSql): OutputSql | undefined {
+    return this.statementAltLib.get(input);
   }
 
   query(input: Input): Output {
     const { itemIDs, libId } = input;
+    const get = libId === 1 ? this.getMainLib : this.getAltLib;
     const query = (this.trxCache[libId] ??= this.database.transaction(
       (itemIDs: number[]) =>
         itemIDs.reduce((rec, itemID) => {
-          const result = this.get({ itemID, libId });
+          const result = get.call(this, { itemID, libId });
           if (!result) return rec;
           rec[itemID] = result.citekey;
           return rec;
