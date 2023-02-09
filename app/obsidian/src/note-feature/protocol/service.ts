c4cc04b215b099fe28e6ffd50fa450e9923b19e3
aidenlx <31102694+aidenlx@users.noreply.github.com>
2023-02-09 16:07:30 +0800

    feat(obsidian/protocol): support create note from zotero docItem
    TODO: support multiple items

--- app/obsidian/src/note-feature/protocol/service.ts
+++ app/obsidian/src/note-feature/protocol/service.ts
@@ -28,7 +28,27 @@ export class ProtocolHandler extends Service {
     }
     await this.plugin.noteFeatures.openNote(query.items[0]);
   }
-  async onZtExport(_query: AnnotationsQuery | ItemsQuery) {
-    new Notice("Not implemented yet");
+  async onZtExport(query: AnnotationsQuery | ItemsQuery) {
+    if (query.type === "annotation") {
+      new Notice("Not implemented yet");
+      return;
+    }
+    if (query.items.length < 1) {
+      new Notice("No items to open");
+    } else if (query.items.length > 1) {
+      new Notice("Multiple items not yet supported");
+    }
+    const { libraryID, id } = query.items[0];
+    const [docItem] = await this.plugin.databaseAPI.getItems([[id, libraryID]]);
+    if (!docItem) {
+      new Notice("Item not found: " + id);
+      return;
+    }
+    const notePath = await this.plugin.noteFeatures.createNoteForDocItemFull(
+      docItem,
+    );
+    await this.plugin.app.workspace.openLinkText(notePath, "", false, {
+      active: true,
+    });
   }
 }
