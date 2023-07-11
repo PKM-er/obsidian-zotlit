53fbfdff93383b540236686a1ecf1622582a0cfd
aidenlx <31102694+aidenlx@users.noreply.github.com>
2023-07-11 10:00:42 +0800

    feat(docs): migrate to nextra

--- docs/docs/how-to/index.md
+++
@@ -1,9 +0,0 @@
----
-sidebar_position: 1
----
-
-# How-To Guide
-
-WIP
-
-Contribution welcome!
--- docs/src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Redirect } from "@docusaurus/router";
-import React from "react";
-
-const Home = () => {
-  return <Redirect to="/overview" />;
-};
-
-export default Home;
