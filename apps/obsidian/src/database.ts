7d0fd184e979187ebf6908d6d6575570eba9e4c9
aidenlx <31102694+aidenlx@users.noreply.github.com>
2025-05-24 22:01:39 +0800

    feat(app/obsidian): database loading

---
+++ apps/obsidian/src/database.ts
@@ -0,0 +1,80 @@
+import workerCode from "inline:@zotlit/database/worker";
+import type {
+  initZotero,
+  initBetterBibtex,
+  init,
+  getAllLibraries,
+} from "@zotlit/database";
+import { useWorkerFns } from "worker-fn";
+import type { RemoteFns } from "@mys-x/m-rpc";
+
+type DatabaseFns = RemoteFns<{
+  init: typeof init;
+  initZotero: typeof initZotero;
+  initBetterBibtex: typeof initBetterBibtex;
+  getAllLibraries: typeof getAllLibraries;
+}>;
+
+import { join } from "node:path";
+import type ZotLitPlugin from "./zt-main";
+
+export class Database implements Disposable {
+  fns: DatabaseFns;
+  #worker: Worker;
+  plugin;
+  constructor(plugin: ZotLitPlugin) {
+    this.plugin = plugin;
+    using workerUrl = createWorkerURL(workerCode);
+    this.#worker = new Worker(workerUrl.href, {
+      type: "module",
+      name: "Zotlit Database Worker",
+    });
+    // biome-ignore lint/correctness/useHookAtTopLevel: <explanation>
+    this.fns = useWorkerFns(this.#worker);
+  }
+
+  [Symbol.dispose]() {
+    this.#worker.terminate();
+  }
+
+  async init() {
+    const { "database.zotero-data-dir": zoteroDataDir } =
+      await this.plugin.settings.loaded;
+    const manifest = this.plugin.manifest as unknown as {
+      versions: Record<string, unknown>;
+    };
+    const dbDriverVersion = manifest.versions?.["better-sqlite3"];
+    if (!dbDriverVersion || typeof dbDriverVersion !== "string") {
+      throw new Error(
+        "better-sqlite3 version is not specified in manifest.json",
+      );
+    }
+    const dbDriverPath = join(
+      require("@electron/remote").app.getPath("userData"),
+      `better-sqlite3-${dbDriverVersion}.node`,
+    );
+    await this.fns.init({
+      dbPaths: {
+        zotero: join(zoteroDataDir, "zotero.sqlite"),
+        betterBibtex: join(zoteroDataDir, "better-bibtex.sqlite"),
+      },
+      nativeBinding: dbDriverPath,
+    });
+    await this.fns.initZotero();
+    try {
+      await this.fns.initBetterBibtex();
+    } catch (e) {
+      console.error("Failed to load Better BibTeX database", e);
+    }
+  }
+}
+
+function createWorkerURL(code: string): { href: string } & Disposable {
+  const blob = new Blob([code], { type: "text/javascript" });
+  return {
+    href: URL.createObjectURL(blob),
+    [Symbol.dispose]() {
+      URL.revokeObjectURL(this.href);
+    },
+  };
+}
--- apps/obsidian/src/zt-main.ts
+++ apps/obsidian/src/zt-main.ts
@@ -3,12 +3,16 @@ import "core-js/proposals/explicit-resource-management";
 import { Plugin } from "obsidian";
 import ZotlitSettings from "./settings/registry";
 import { ZotlitSettingTab } from "./settings/tab";
+import { Database } from "./database";
 
 export default class ZotLit extends Plugin {
   settings = new ZotlitSettings(this);
+  database = new Database(this);
   async onload() {
     console.log("loading zotlit");
     await this.settings.loadSettings();
     this.addSettingTab(new ZotlitSettingTab(this.app, this));
+    await this.database.init();
+    console.log("zotlit loaded");
   }
 }
